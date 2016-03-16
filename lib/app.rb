24d67a53f22745f73f0be13307df1a7e5b331f4b
Casey Courtney <casey.courtney@gmail.com>
2016-03-16 22:08:27 +0700

    Initial commit - setup

---
+++ lib/app.rb
@@ -0,0 +1,34 @@
+# Get path to products.json, read the file into a string,
+# and transform the string into a usable hash
+def setup_files
+    path = File.join(File.dirname(__FILE__), '../data/products.json')
+    file = File.read(path)
+    $products_hash = JSON.parse(file)
+    $report_file = File.new("report.txt", "w+")
+end
+
+setup_files
+
+$report_file
+
+# Print "Sales Report" in ascii art
+
+# Print today's date
+
+# Print "Products" in ascii art
+
+# For each product in the data set:
+	# Print the name of the toy
+	# Print the retail price of the toy
+	# Calculate and print the total number of purchases
+	# Calculate and print the total amount of sales
+	# Calculate and print the average price the toy sold for
+	# Calculate and print the average discount (% or $) based off the average sales price
+
+# Print "Brands" in ascii art
+
+# For each brand in the data set:
+	# Print the name of the brand
+	# Count and print the number of the brand's toys we stock
+	# Calculate and print the average price of the brand's toys
+	# Calculate and print the total sales volume of all the brand's toys combined
